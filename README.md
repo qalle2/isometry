e9b8c126dba0a887f30fa5ceab1ba124b0a27e89
Kalle <qalle85@gmail.com>
2023-10-31 14:38:11 +0200

    allow different minicube sizes

--- README.md
+++ README.md
@@ -1,23 +1,32 @@
 # isometry
-Draw an isometric image consisting of cubes according to instructions in a text file.
+Draw an isometric image consisting of cubes according to instructions in a text file. Requires the [Pillow](https://python-pillow.org) module.
 
 ## Command line arguments
 * input file (see below)
-* output file (PNG)
+* output file (PNG, RGB without alpha)
 
 ## Input file
 * Encoding: ASCII.
 * Case insensitive.
 * Empty lines are ignored.
-* Specify width:  `W` immediately followed by an integer.
-* Specify length: `L` immediately followed by an integer.
+* Specify width: `W` immediately followed by an integer.
+* Specify depth: `D` immediately followed by an integer.
 * Specify height: `H` immediately followed by an integer.
 * Other lines:
-  * Each group of *length* lines describes a horizontal layer.
-  * There are *height* &times; *length* lines in total.
+  * Each group of *depth* lines describes a horizontal layer.
+  * There are *height* &times; *depth* lines in total.
   * Bottom layer first.
   * Rear line first within each layer.
   * Each text line starts with `|`.
   * `|` is followed by up to *width* characters, excluding newline.
   * After `|`, spaces denote "no cube" and any other character denotes "cube".
   * Missing trailing spaces are assumed to be "no cube".
+
+## More settings
+The constants at the beginning of the program can be edited. You can change:
+* the dimensions of the building blocks (only the combinations corresponding to `minicube-*.png` files are allowed; see below)
+* the margins
+* the background color
+
+## Other files
+* `minicube-WxHxD.png`: the cubes used as building blocks; `W`/`H`/`D` are width/height/depth in pixels
--- isometry.py
+++ isometry.py
@@ -1,8 +1,21 @@
-# create a PNG with an isometric drawing
+# Draw an isometric image consisting of cubes.
 
-CUBE_IMAGE = "minicube.png"  # RGBA, 25*25 pixels
+# the building block (a single cube)
+CUBE_WIDTH  = 16  # width minus one
+CUBE_HEIGHT = 16  # height minus one
+CUBE_DEPTH  = 8  # depth minus one
+CUBE_FILE = f"minicube-{CUBE_WIDTH}x{CUBE_HEIGHT}x{CUBE_DEPTH}.png"
+CUBE_IMG_WIDTH  = CUBE_WIDTH  + CUBE_DEPTH + 1  # image width
+CUBE_IMG_HEIGHT = CUBE_HEIGHT + CUBE_DEPTH + 1  # image height
 
-import re, sys
+# horizontal/vertical margin on each side of the final image, in pixels
+HORZ_MARGIN = CUBE_WIDTH
+VERT_MARGIN = CUBE_HEIGHT
+
+# background color of final image (red, green, blue)
+BACKGROUND_COLOR = (255, 255, 255)
+
+import os, re, sys
 try:
     from PIL import Image
 except ImportError:
@@ -19,7 +32,7 @@ def get_lines(filename):
 
 def string_to_int(stri, width):
     # interpret string as binary number;
-    # space = 0, non-space = 1, pad to width with zeros;
+    # space = 0, non-space = 1, pad to width with trailing zeros;
     # e.g. (" ab", 5) -> 0b01100
     return int(re.sub(r"[^ ]", r"1", stri).ljust(width).replace(" ", "0"), 2)
 
@@ -29,14 +42,15 @@ def main():
     (inputFile, outputFile) = sys.argv[1:]
 
     # parse input file
+    width = depth = height = None
     lines = []
     for line in get_lines(inputFile):
         line = line.upper()
         try:
             if line.startswith("W"):
                 width = int(line[1:])
-            elif line.startswith("L"):
-                length = int(line[1:])
+            elif line.startswith("D"):
+                depth = int(line[1:])
             elif line.startswith("H"):
                 height = int(line[1:])
             elif line.startswith("|"):
@@ -46,10 +60,12 @@ def main():
         except ValueError:
             sys.exit("Not an integer: " + line[1:])
 
-    if min(width, length, height) < 1:
+    if any(d is None for d in (width, depth, height)):
+        sys.exit("Must define all of width, depth and height.")
+    if min(width, depth, height) < 1:
         sys.exit("Each dimension must be 1 or greater.")
-    if len(lines) != height * length:
-        sys.exit("There must be {height*length} lines starting with '|'.")
+    if len(lines) != height * depth:
+        sys.exit("There must be {height*depth} lines starting with '|'.")
     if max(len(l) for l in lines) > width:
         sys.exit("There must be no lines longer than {width}.")
 
@@ -58,31 +74,51 @@ def main():
 
     # convert into a tuple of tuples of ints
     lines = tuple(
-        tuple(lines[i:i+length])
-        for i in range(0, height * length, length)
+        tuple(lines[i:i+depth])
+        for i in range(0, height * depth, depth)
     )
 
-    #print(f"{width=}, {length=}, {height=}")
-    #print("Lines:", lines)
+    # dimensions of final image
+    imgWidth  = width  * CUBE_WIDTH  + depth * CUBE_DEPTH + 2 * HORZ_MARGIN
+    imgHeight = height * CUBE_HEIGHT + depth * CUBE_DEPTH + 2 * VERT_MARGIN
 
-    imgWidth  = width  * 16 + length * 8 + 2 * 8
-    imgHeight = height * 16 + length * 8 + 2 * 8
+    if not os.path.isfile(CUBE_FILE):
+        sys.exit(f"{CUBE_FILE} not found.")
 
-    # read cube image (RGBA, 25*25 pixels)
-    with open(CUBE_IMAGE, "rb") as handle:
+    # read the image of the building block
+    with open(CUBE_FILE, "rb") as handle:
         handle.seek(0)
         cubeImage = Image.open(handle)
+        if cubeImage.mode != "RGBA":
+            sys.exit(f"{CUBE_FILE} must be in RGBA format.")
+        if cubeImage.width != CUBE_IMG_WIDTH:
+            sys.exit(f"{CUBE_FILE} must be {CUBE_IMG_WIDTH} pixels wide.")
+        if cubeImage.height != CUBE_IMG_HEIGHT:
+            sys.exit(f"{CUBE_FILE} must be {CUBE_IMG_HEIGHT} pixels tall.")
 
         # create image
-        image = Image.new("RGBA", (imgWidth, imgHeight))
+        image = Image.new(
+            "RGBA", (imgWidth, imgHeight), BACKGROUND_COLOR + (255,)
+        )
 
-        for y in range(length):         # +Y = towards viewer
+        for y in range(depth):          # +Y = towards viewer
             for z in range(height):     # +Z = up
                 for x in range(width):  # +X = right
                     if lines[z][y] & (1 << (width - 1 - x)):
-                        imgX = 8 + x * 16 + (length - 1 - y) * 8
-                        imgY = 8 + (height - 1 - z) * 16 + y * 8
+                        imgX = (
+                            HORZ_MARGIN
+                            + x * CUBE_WIDTH
+                            + (depth - 1 - y) * CUBE_DEPTH
+                        )
+                        imgY = (
+                            VERT_MARGIN
+                            + (height - 1 - z) * CUBE_HEIGHT
+                            + y * CUBE_DEPTH
+                        )
                         image.alpha_composite(cubeImage, dest=(imgX, imgY))
+
+    # remove alpha channel
+    image = image.convert("RGB")
 
     # save image
     with open(outputFile, "wb") as handle:
