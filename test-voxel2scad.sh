87dbc791e85643ca4bbf0136113eacb0f86bcda7
Kalle <qalle85@gmail.com>
2025-05-03 04:37:09 +0300

    add mega blastoise

--- test-voxel2scad.sh
+++ test-voxel2scad.sh
@@ -6,5 +6,8 @@ grep "optimised"                          test-out/eevee.scad
 python3 voxel2scad.py test-in/lucario.txt > test-out/lucario.scad
 grep "optimised"                            test-out/lucario.scad
 
+python3 voxel2scad.py test-in/megablastoise.txt > test-out/megablastoise.scad
+grep "optimised"                                  test-out/megablastoise.scad
+
 python3 voxel2scad.py test-in/vaporeon.txt > test-out/vaporeon.scad
 grep "optimised"                             test-out/vaporeon.scad
