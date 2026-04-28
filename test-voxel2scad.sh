4442773d5ab369ca92751413a70caf83dc1a24f3
Kalle <qalle85@gmail.com>
2026-04-28 11:33:27 +0300

    update test

--- test-voxel2scad.sh
+++ test-voxel2scad.sh
@@ -1,16 +1,14 @@
 rm -f test-out/*.scad
 
-python3 voxel2scad.py test-in/eevee.txt > test-out/eevee.scad
-grep "optimised"                          test-out/eevee.scad
+python3 voxel2scad.py test-in/eevee.txt          > test-out/eevee.scad
+python3 voxel2scad.py test-in/lapras.txt         > test-out/lapras.scad
+python3 voxel2scad.py test-in/litten.txt         > test-out/litten-default.scad
+python3 voxel2scad.py test-in/litten.txt 1       > test-out/litten-1colour.scad
+python3 voxel2scad.py test-in/litten.txt 0 1 0   > test-out/litten-nocombine.scad
+python3 voxel2scad.py test-in/litten.txt 0 0 1 0 > test-out/litten-nooverlap.scad
+python3 voxel2scad.py test-in/lucario.txt        > test-out/lucario.scad
+python3 voxel2scad.py test-in/vaporeon.txt       > test-out/vaporeon.scad
 
-python3 voxel2scad.py test-in/lucario.txt > test-out/lucario.scad
-grep "optimised"                            test-out/lucario.scad
-
-python3 voxel2scad.py test-in/megablastoise.txt > test-out/megablastoise.scad
-grep "optimised"                                  test-out/megablastoise.scad
-
-python3 voxel2scad.py test-in/megablastoise-filled.txt > test-out/megablastoise-filled.scad
-grep "optimised"                                         test-out/megablastoise-filled.scad
-
-python3 voxel2scad.py test-in/vaporeon.txt > test-out/vaporeon.scad
-grep "optimised"                             test-out/vaporeon.scad
+# these are slow so test them last
+python3 voxel2scad.py test-in/megablastoise.txt  > test-out/megablastoise.scad
+python3 voxel2scad.py test-in/megacharizardy.txt > test-out/megacharizardy.scad
